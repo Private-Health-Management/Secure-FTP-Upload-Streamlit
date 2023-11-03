bd54d7e58f43803c318f0dfe26351cbf5a0cb9bf
Teddy Caulton <118858063+teddycaultonhakkoda@users.noreply.github.com>
2023-11-03 11:59:16 -0700

    changed theme and titling

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import paramiko
 st.set_page_config(page_title='PHM Secure-Send', page_icon="✉️")
 
-email = st.text_input('Please enter a valid entry string')
+email = st.text_input('Please enter a valid email')
 if email in st.secrets["email_list"]:
     col1, mid, col2 = st.columns([1,2,2])
     with col1:
